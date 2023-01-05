834ec1c68d2793c0ec345499869e975b53b45f5d
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-05 19:16:14 +0100

    button_color_change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
-let buttonColor = () =>{
-    let count;
-    console.log(count=+1);
+let count = 0;
+let button = document.getElementById("button");
+let counter = () =>{
+    count ++ ;
+    console.log(count);
+    if(count % 2 !== 0){
+        button.style.backgroundColor="black";
+        button.style.color="white";
+    }
+    else{
+        button.style.backgroundColor="white";
+        button.style.color="black";
+    }
 }
+
+
+
+
