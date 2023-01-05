8e4396a60fc47fed6d451b985d1ddbbaea9a5f65
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-05 22:13:59 +0100

    messy_but_finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,25 @@
 let countButton = 0;
 let button = document.getElementById("buttonS");
+let buttonL= document.getElementById("buttonL");
 let counter = () =>{
     countButton ++ ;
     console.log(countButton);
     if(countButton % 2 !== 0){
         button.style.backgroundColor="black";
         button.style.color="white";
+
+        buttonL.style.backgroundColor="black";
+        buttonL.style.color="white";
     }
     else{
         button.style.backgroundColor="white";
         button.style.color="black";
+
+        buttonL.style.backgroundColor="white";
+        buttonL.style.color="black";
     }
 }
+
 
 
 
